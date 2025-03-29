713ece1cb8218ebae65669d392b31587d4ddaf33
Olexandr Yurkevych <olexandr.yurkevych@gmail.com>
2025-03-29 09:16:02 +0200

    chore: fix superbase server

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,30 +1,42 @@
-import type { cookies } from 'next/headers';
-import type { CookieOptions } from '@supabase/ssr';
-import { createServerClient } from '@supabase/ssr';
-import type { SupabaseClient } from '@supabase/supabase-js';
+import type { cookies } from "next/headers";
+import type { CookieOptions } from "@supabase/ssr";
+import { createServerClient } from "@supabase/ssr";
+import type { SupabaseClient } from "@supabase/supabase-js";
 
-import { config } from '@/config';
+import { config } from "@/config";
 
-type ResponseCookie = Pick<CookieOptions, 'httpOnly' | 'maxAge' | 'priority'>;
+type ResponseCookie = Pick<CookieOptions, "httpOnly" | "maxAge" | "priority">;
 
-export function createClient(cookieStore: ReturnType<typeof cookies>): SupabaseClient {
+export function createClient(
+  cookieStore: ReturnType<typeof cookies>
+): SupabaseClient {
   return createServerClient(config.supabase.url!, config.supabase.anonKey!, {
+    cookieOptions: {
+      name: "sb",
+      path: "/",
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 60 * 60 * 24 * 7,
+    },
     cookies: {
-      get(name: string) {
-        return cookieStore.get(name)?.value;
+      async get(name: string) {
+        const store = await cookieStore;
+        return store.get(name)?.value;
       },
-      set(name: string, value: string, options: CookieOptions) {
+      async set(name: string, value: string, options: CookieOptions) {
         try {
-          cookieStore.set({ name, value, ...(options as Partial<ResponseCookie>) });
+          const store = await cookieStore;
+          store.set({ name, value, ...options });
         } catch (error) {
           // The `set` method was called from a Server Component.
           // This can be ignored if you have middleware refreshing
           // user sessions.
         }
       },
-      remove(name: string, options: CookieOptions) {
+      async remove(name: string, options: CookieOptions) {
         try {
-          cookieStore.set({ name, value: '', ...(options as Partial<ResponseCookie>) });
+          const store = await cookieStore;
+          store.set({ name, value: "", ...options });
         } catch (error) {
           // The `delete` method was called from a Server Component.
           // This can be ignored if you have middleware refreshing
