926f0b2f5fd4bc9eb08e06c8afd6f3d36147d44a
Olexandr Yurkevych <olexandr.yurkevych@gmail.com>
2025-04-17 08:03:02 +0300

    fix: test api types

--- src/components/test-api-key.tsx
+++ src/components/test-api-key.tsx
@@ -1,9 +1,7 @@
 "use client";
 import {FC} from 'react';
 
-type TestApiKeyProps = {};
-
-export const TestApiKey: FC<TestApiKeyProps> = ({}) => {
+export const TestApiKey: FC = () => {
     console.log("TestApiKey", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
     return (
         <div>
