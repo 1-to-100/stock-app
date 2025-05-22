d4bc14f64980cf62417a7f9f3afe01682874ae7e
odycceus <odycceus+git@gmail.com>
2025-05-22 11:08:53 +0300

    chore: add session invite check and update password page 3

--- src/app/auth/supabase/set-new-password/page.tsx
+++ src/app/auth/supabase/set-new-password/page.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 
-import { config } from "@/config";
-import { SplitLayout } from "@/components/auth/split-layout";
-import { UpdatePasswordForm } from "@/components/auth/supabase/update-password-form";
+import {config} from "@/config";
+import {SplitLayout} from "@/components/auth/split-layout";
+import {UpdatePasswordForm} from "@/components/auth/supabase/update-password-form";
 import {CheckSessionInvite} from "@/components/auth/supabase/check-session-invite";
+import {AuthGuard} from "@/components/auth/auth-guard";
 
 export const metadata = {
   title: `Set new password | ${config.site.name}`,
@@ -12,10 +13,12 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <SplitLayout>
-      <CheckSessionInvite>
-        <UpdatePasswordForm title={'Set new password'}/>
-      </CheckSessionInvite>
-    </SplitLayout>
+    <AuthGuard>
+      <SplitLayout>
+        <CheckSessionInvite>
+          <UpdatePasswordForm title={'Set new password'}/>
+        </CheckSessionInvite>
+      </SplitLayout>
+    </AuthGuard>
   );
 }
--- src/components/auth/supabase/check-session-invite.tsx
+++ src/components/auth/supabase/check-session-invite.tsx
@@ -10,6 +10,11 @@ export const CheckSessionInvite = ({children}: { children: ReactNode }) => {
   const [message, setMessage] = useState<string | null>('');
 
   useEffect(() => {
+    const params = new URLSearchParams(window.location.hash.slice(1));
+    const access_token = params.get('access_token')
+    const refresh_token = params.get('refresh_token')
+    console.log('access_token', access_token);
+
     supabaseClient.auth.getSession().then((data) => {
       setMessage(data?.data?.session ? null : 'Invalid or expired invitation link. Please request a new invitation.');
       console.log('xxx', data);
