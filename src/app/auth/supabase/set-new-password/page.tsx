cd984d78d5c78c7a2c2db898fa2c7afd585b6417
odycceus <odycceus+git@gmail.com>
2025-06-25 20:08:32 +0300

    feat: improve password reset flow with loading state and user checks 3

--- src/app/auth/supabase/set-new-password/page.tsx
+++ src/app/auth/supabase/set-new-password/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
   return (
     <SplitLayout>
       <CheckSessionInvite>
-        <UpdatePasswordForm title={'Set new password'}/>
+        <UpdatePasswordForm />
       </CheckSessionInvite>
     </SplitLayout>
   );
